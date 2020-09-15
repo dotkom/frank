acec20b733aa8a605c2541fd98196ba08e9a3e6e
monikahm <monika.muzyk@hotmail.com>
2020-09-15 13:23:00 +0200

    Move circle calculations to constants

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./Circle.css";
+import {width, pathData, number_people} from '../constants/circlePoints'
 
 export default function Circle() {
   const [user, setUser] = useState([]);
@@ -10,46 +11,8 @@ export default function Circle() {
       .then((res) => res.json())
       .then((res) => setUser(res.results[0]));
   }, []);
-  const width = 500;
-  const r = 700 / (Math.PI * 2);
-  const d = r * 2;
-  const y = (width - d) / 2;
-  const pathData = [
-    "M",
-    width / 2,
-    y,
-    "a",
-    r,
-    r,
-    0,
-    0,
-    1,
-    0,
-    d,
-    "a",
-    r,
-    r,
-    0,
-    0,
-    1,
-    0,
-    -d,
-  ];
 
-  const number_people = [
-    [700 / 12, 700],
-    [(700 / 12) * 2, 700],
-    [(700 / 12) * 3, 700],
-    [(700 / 12) * 4, 700],
-    [(700 / 12) * 5, 700],
-    [(700 / 12) * 6, 700],
-    [(700 / 12) * 7, 700],
-    [(700 / 12) * 8, 700],
-    [(700 / 12) * 9, 700],
-    [(700 / 12) * 10, 700],
-    [(700 / 12) * 11, 700],
-    [(700 / 12) * 12, 700],
-  ];
+
   console.log(click);
 
   return (
