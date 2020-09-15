5062a67e03f1e4a28c5f88995e224313ddb804bd
monikahm <monika.muzyk@hotmail.com>
2020-09-15 13:11:48 +0200

    Animation update

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -4,6 +4,7 @@ import "./Circle.css";
 export default function Circle() {
   const [user, setUser] = useState([]);
   const [click, setClick] = useState(0);
+  const [progress, setProgress] = useState(0)
   useEffect(() => {
     fetch("https://online.ntnu.no/api/v1/users/?rfid=0260434848")
       .then((res) => res.json())
@@ -47,7 +48,7 @@ export default function Circle() {
     [(700 / 12) * 9, 700],
     [(700 / 12) * 10, 700],
     [(700 / 12) * 11, 700],
-    [(700 / 12) * 12, 700]
+    [(700 / 12) * 12, 700],
   ];
   console.log(click);
 
@@ -55,13 +56,21 @@ export default function Circle() {
     <div className="container">
       <svg height={width} width={width} className="svg">
         <path
+          d={pathData.join(" ")}
+          fill="transparent"
+          stroke="#242b2e"
+          strokeWidth="20"
+        ></path>
+        <path
           className="circle"
           d={pathData.join(" ")}
           fill="transparent"
           stroke="#0D5474"
           strokeWidth="20"
-          strokeDasharray={number_people[click]}
-
+          strokeDasharray={number_people[progress]}
+          onClick={() => {setClick(1); setProgress(progress +1);}}
+          onAnimationEnd={() => setClick(0)}
+          click={click}
         ></path>
         <text
           className="text"
@@ -69,7 +78,6 @@ export default function Circle() {
           y="50%"
           fill="white"
           textAnchor="middle"
-          onClick={() => setClick(click + 1)}
         >
           {user.first_name} {user.last_name}
         </text>
